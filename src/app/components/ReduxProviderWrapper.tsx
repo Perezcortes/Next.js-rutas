67a4f0b5b04b2293f080ade2f73e1ad7b3eae529
Perezcortes <9531447599a@gmail.com>
2025-05-27 11:31:59 -0600

    ReduxProviderWrapper para el uso de <Provider> y evitar problemas

---
+++ src/app/components/ReduxProviderWrapper.tsx
@@ -0,0 +1,9 @@
+// src/components/ReduxProviderWrapper.tsx
+'use client';
+
+import { Provider } from 'react-redux';
+import { store } from '../../redux/store';
+
+export default function ReduxProviderWrapper({ children }: { children: React.ReactNode }) {
+  return <Provider store={store}>{children}</Provider>;
+}
